8d68bdfbfe07b30625f7385d3c48ce636df01a7d
Jonathan <jtlimads@gmail.com>
2025-11-18 11:19:42 -0300

    primeiras telas

--- view/tela_sistema.py
+++ view/tela_sistema.py
@@ -1,6 +1,53 @@
-class TelaSistema():
+import FreeSimpleGUI as sg
 
+class TelaSistema():
+    def __init__(self):
+        self.__window = None
+        self.init_components()
+    
     def menu(self):
+        self.init_components()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if values['5']:
+            opcao = 5
+        if values['6']:
+            opcao = 6
+        # cobre os casos de voltar, não clicar em nada e fechar janela, ou clicar cancelar
+        if values['0'] or button in (None,'Cancelar'):
+            opcao = 0
+        self.close()
+        return opcao
+    
+    def close(self):
+        self.__window.Close()
+    
+    def init_components(self):
+        #sg.theme_previewer()
+        sg.ChangeLookAndFeel('DarkBlue')
+        layout = [
+            [sg.Text('EKMass: a system for simulating contaminant transport by coupled flows', font=("Courier", 18))],
+            [sg.Text('Escolha sua opção', font=("Helvica",15))],
+            [sg.Radio('Usuários',"RD1", key='1')],
+            [sg.Radio('Solos',"RD1", key='2')],
+            [sg.Radio('Espécies químicas',"RD1", key='3')],
+            [sg.Radio('Células experimentais',"RD1", key='4')],
+            [sg.Radio('Condições iniciais e de contorno',"RD1", key='5')],
+            [sg.Radio('Simulação',"RD1", key='6')],
+            [sg.Radio('Finalizar sistema',"RD1", key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Sistema de livros').Layout(layout)
+
+    '''def menu(self):
         print(">>> SISTEMA DE SIMULAÇÃO DE EXPERIMENTOS COM ELETROCINÉTICA <<<")
         print("1 - Usuários")
         print("2 - Solos")
@@ -11,7 +58,7 @@ class TelaSistema():
         print("0 - Finalizar")
     
         opcao = int(input("Selecione uma opção: "))
-        return opcao
+        return opcao'''
     
     def imprime_mensagem(self, mensagem):
         print(mensagem)
--- view/tela_solo.py
+++ view/tela_solo.py
@@ -1,17 +1,62 @@
+import FreeSimpleGUI as sg
+
 class TelaSolo():
+    def __init__(self):
+        self.__window = None
+        self.init_opcoes()
 
     def mostra_menu(self):
-        print("=== MENU SOLOS ===")
-        print("1 - Cadastrar solo")
-        print("2 - Retornar solo")
-        print("3 - Mostra solos")
-        print("4 - Deleta solo")
-        print("0 - Voltar")
-        return int(input("Escolha: "))
+        self.init_opcoes()
+        button, values = self.open()
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        # cobre os casos de Retornar, fechar janela, ou clicar cancelar
+        #Isso faz com que retornemos a tela do sistema caso qualquer uma dessas coisas aconteca
+        if values['0'] or button in (None, 'Cancelar'):
+            opcao = 0
+        self.close()
+        return opcao
+    
+    def init_opcoes(self):
+        #sg.theme_previewer()
+        sg.ChangeLookAndFeel('LightBrown1')
+        layout = [
+            [sg.Text('-------- SOLOS ----------', font=("Helvica", 25))],
+            [sg.Text('Escolha sua opção', font=("Helvica", 15))],
+            [sg.Radio('Incluir solo', "RD1", key='1')],
+            [sg.Radio('Alterar solo', "RD1", key='2')],
+            [sg.Radio('Listar solos', "RD1", key='3')],
+            [sg.Radio('Deletar solos', "RD1", key='4')],
+            [sg.Radio('Retornar', "RD1", key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Registro de solos').Layout(layout)
     
     def imprime_mensagem(self, mensagem):
         print(mensagem)
 
     def coleta_codigo(self):
-        codigo = input("Digite o código do solo: ")
-        return codigo
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+            [sg.Text('-------- DADOS DO SOLO ----------', font=("Courrier", 20))],
+            [sg.Text('Código do solo:', size=(15, 1)), sg.InputText('', key='código')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Registro de solos').Layout(layout)
+        button, values = self.open()
+        codigo = values['código']
+        self.close()
+        return {"código": codigo}
+    
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
